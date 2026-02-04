8c41366d6f824007a23369eeb037694be45a7b26
Aaron Alejandro Martinez Serrano <za230120317@zapopan.tecmm.edu.mx>
2026-02-04 17:05:48 -0600

    holamundo

---
+++ 02-hola-mundo/230120317.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+	cout << "Hola mundo!" << endl;
+	cout << "Aaron" << endl;
+	cout << "Numero de control: 230120317" << endl;
+	return 0;
+}
+
